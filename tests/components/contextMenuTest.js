2eb0ffe82572e21e4420041cb9dfd5f676bddbe8
Jahn08 <jahn_08@mail.ru>
2019-03-22 21:34:43 +0300

    Assuring of colour changes when clicking on menu radio items.

--- tests/components/contextMenuTest.js
+++ tests/components/contextMenuTest.js
@@ -22,7 +22,8 @@ describe('components/ContextMenu', () => {
             assert.strictEqual(itemOptions.filter(i => i.type === 'radio').length, 6);
         });
 
-        const testClickingOnMenuItem = (menuCallbackNameToTest, filterMenuItemCallback) => {
+        const testClickingOnMenuItem = (menuCallbackNameToTest, filterMenuItemCallback, 
+            onClickedCallback = null) => {
             const browserMocked = mockBrowser();
             browserMocked.setBrowserTab();
             
@@ -39,9 +40,19 @@ describe('components/ContextMenu', () => {
             return Promise.all(foundItems.map(item => {
                 assert(item.onclick);
                 
-                return new Promise(async resolve => {
-                    await item.onclick({ menuItemId: Randomiser.getRandomNumberUpToMax() });
-                    resolve();
+                return new Promise(async (resolve, reject) => {
+                    const tabItemInfo = { menuItemId: Randomiser.getRandomNumberUpToMax() };
+                    try {
+                        await item.onclick(tabItemInfo);
+
+                        if (onClickedCallback)
+                            onClickedCallback(tabItemInfo, menu);
+    
+                        resolve();
+                    }
+                    catch (ex) {
+                        reject(ex);
+                    }
                 }).then(() => {
                     const tabQueries = browserMocked.tabQueries;
                     assert.strictEqual(tabQueries.length, foundItems.length);
@@ -55,30 +66,34 @@ describe('components/ContextMenu', () => {
                 { id: 'unmark', callbackName: 'onUnmarking' }]
                     .map(o => testClickingOnMenuItem(o.callbackName, i => i.id === o.id))));
 
-        it('should run event callbacks while clicking on menu items to change colours', 
-            () => testClickingOnMenuItem('onChangingColour', i => i.type === 'radio'));
-    });
+        it('should run event callbacks and change the marker colour while clicking on menu radio items', () => { 
+            const menuColourIds = [];
+            const changedColourIds = [];
 
-    it('should change visibility of button items in a context menu', () => {
-        const browserMocked = mockBrowser();
-        const contextMenu = new ContextMenu();
+            return testClickingOnMenuItem('onChangingColour', i => i.type === 'radio',
+                (menuItemInfo, menu) => {
+                    menuColourIds.push(menuItemInfo.menuItemId);
+                    changedColourIds.push(menu.currentColourClass);
+                }).then(() => assert(changedColourIds.every(cl => menuColourIds.includes(cl))));
+        });
 
-        contextMenu.hideMarkingBtn();
-        contextMenu.hideUnmarkingBtn();
+        it('should change visibility of button items in a context menu', () => {
+            const browserMocked = mockBrowser();
+            const contextMenu = new ContextMenu();
 
-        const btnOptions = browserMocked.menuOptions
-            .filter(i => i.type === 'normal' && i.visible !== undefined);
-        assert.strictEqual(btnOptions.length, 2);
-        
-        const assertItemsVisibility = (visible) => assert(btnOptions.every(b => b.visible === visible));
-        assertItemsVisibility(false);
+            contextMenu.hideMarkingBtn();
+            contextMenu.hideUnmarkingBtn();
 
-        contextMenu.showMarkingBtn();
-        contextMenu.showUnmarkingBtn();
-        assertItemsVisibility(true);
-    });
+            const btnOptions = browserMocked.menuOptions
+                .filter(i => i.type === 'normal' && i.visible !== undefined);
+            assert.strictEqual(btnOptions.length, 2);
+            
+            const assertItemsVisibility = (visible) => assert(btnOptions.every(b => b.visible === visible));
+            assertItemsVisibility(false);
 
-    describe('#currentColourClass', () => {
-        it('should return a current colour class, chosen in a menu');
+            contextMenu.showMarkingBtn();
+            contextMenu.showUnmarkingBtn();
+            assertItemsVisibility(true);
+        });
     });
 });
