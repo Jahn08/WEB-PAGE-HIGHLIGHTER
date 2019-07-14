8f6f96c3f8b89e3542a4ce63d5e9f77343105d8d
Jahn08 <jahn_08@mail.ru>
2019-07-14 20:36:51 +0300

    Preferences cannot be saved until the page is loaded.

--- background_scripts/preferencesInitialisation.js
+++ background_scripts/preferencesInitialisation.js
@@ -1,10 +1,11 @@
 import { Preferences } from '../components/preferences.js';
 
 const preferences = new Preferences();
-
-preferences.load();
-
-document.forms[0].addEventListener('submit', _event => {
-    preferences.save();
-    _event.preventDefault();
+preferences.load().then(() => {
+    document.querySelector('button[type=submit]').disabled = undefined;
+    
+    document.forms[0].addEventListener('submit', _event => {
+        preferences.save();
+        _event.preventDefault();
+    });
 });
