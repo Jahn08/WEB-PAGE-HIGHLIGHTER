fe83692e3ef09104660f61b002f87f9041fabefb
Jahn08 <jahn_08@mail.ru>
2019-08-11 12:29:16 +0300

    Improving readability of the main module.

--- content_scripts/index.js
+++ content_scripts/index.js
@@ -1,194 +1,204 @@
 void function() {
-    let activeNode;
 
-    let domIsPure;
-    let canLoad;
+    class Highlighter {
+        constructor() {
+            this._activeNode;
 
-    const preferences = {};
+            this._domIsPure;
+            this._canLoad;
 
-    const BEFORE_UNLOAD_EVENT = 'beforeunload';
+            this._browserApi = new BrowserAPI();
+            this._browserApi.runtime.onMessage(this._processMessage.bind(this));
 
-    const beforeUnloadEventListener = _event => {
-        if (domIsPure === false)
-            return _event.returnValue = 'You discard all unsaved changes on this page when leaving.';
-    
-        return;
-    };
+            this._pageInfo = new PageInfo();
+            this._initUnloadEvent();
 
-    window.addEventListener(BEFORE_UNLOAD_EVENT, beforeUnloadEventListener);
-
-    const performStorageAction = async callback => {
-        try {
-            await callback();
-
-            canLoad = await pageInfo.canLoad();
-
-            domIsPure = true;
+            document.addEventListener('mousedown', this._setUpContextMenu.bind(this));
         }
-        finally {
-            MessageControl.hide();
-        }
-    };
 
-    const pageInfo = new PageInfo();
+        _initUnloadEvent() {
+            const beforeUnloadEvent = 'beforeunload';
 
-    const save = async () => {
-        try {
-            await pageInfo.save();
-            MessageControl.show('The page has been saved successfully');
-        }
-        catch (err) {
-            alert(`An error occurred while trying to save the page: "${err.toString()}". ` +
-                'Please, consider going to the preferences to remove redundant saved pages');
-        }
-    };
+            const eventCallback = this._warnIfDomIsDirty.bind(this);
+            window.addEventListener(beforeUnloadEvent, eventCallback);
 
-    const load = async () => {
-        MessageControl.show('Page is loading');
-        await pageInfo.load();
-
-        MessageControl.show('The page has been loaded successfully');
-    };
-
-    const browserApi = new BrowserAPI();
-    browserApi.runtime.sendMessage(MessageReceiver.loadPreferences()).then(async settings => {
-        try {
-            Object.assign(preferences, settings);
-
-            if (preferences.shouldWarn === false)
-                window.removeEventListener(BEFORE_UNLOAD_EVENT, beforeUnloadEventListener);
-
-            canLoad = await pageInfo.canLoad();
-
-            if (canLoad && (preferences.shouldLoad || pageInfo.shouldLoad()))
-                await performStorageAction(load);
-        }
-        catch (ex) {
-            console.error('An error occurred while trying to apply the extension preferences: ' + 
-                ex.toString());
-        }
-    });    
-
-    const includeLoadSaveEvents = (msg = null) => {
-        if (domIsPure)
-            return msg;
-
-        if (domIsPure === false)
-            msg = MessageReceiver.combineEvents(msg, 
-                MessageReceiver.setSaveMenuReady());
-
-        if (canLoad)
-            msg = MessageReceiver.combineEvents(msg, 
-                MessageReceiver.setLoadMenuReady());
-
-        return msg;
-    };
-
-    const sleep = milliseconds => {
-        const start = new Date().getTime();
-    
-        for (let i = 0; i < 1e7; i++)
-            if ((new Date().getTime() - start) > milliseconds)
-                break;
-    };
-
-    document.addEventListener('mousedown', _event => {
-        try {
-            if (_event.button !== 2)
-                return true;
-
-            let msg;
-            const curColourClasses = RangeMarker.getColourClassesForSelectedNodes();
-
-            const focusedNode = _event.target;
-
-            if (curColourClasses)
-            {
-                msg = MessageReceiver.combineEvents(MessageReceiver.setMarkMenuReady(curColourClasses), 
-                    MessageReceiver.setAddNoteMenuReady());
-
-                if (curColourClasses.length)
-                    msg = MessageReceiver.combineEvents(msg, MessageReceiver.setUnmarkMenuReady());
-            }
-            else if (RangeMarker.isNodeMarked(focusedNode)) 
-            {
-                msg = MessageReceiver.combineEvents(MessageReceiver.setUnmarkMenuReady(), 
-                    MessageReceiver.setAddNoteMenuReady());
-                activeNode = focusedNode;
-            }
+            this._browserApi.runtime.sendMessage(MessageReceiver.loadPreferences())
+                .then(async settings => {
+                    try {
+                        const preferences = Object.assign({}, settings);
             
-            if (RangeNote.hasNote(focusedNode)) {
-                msg = MessageReceiver.combineEvents(msg, MessageReceiver.setRemoveNoteMenuReady());
-                activeNode = focusedNode;
-            }
+                        if (preferences.shouldWarn === false)
+                            window.removeEventListener(beforeUnloadEvent, eventCallback);
             
-            msg = MessageReceiver.combineEvents(msg, MessageReceiver.addNoteLinks(RangeNote.getNoteLinks()));
-            browserApi.runtime.sendMessage(includeLoadSaveEvents(msg));
-
-            sleep(10);
+                        this._canLoad = await this._pageInfo.canLoad();
+            
+                        if (this._canLoad && (preferences.shouldLoad || this._pageInfo.shouldLoad()))
+                            await this._performStorageAction(this._load);
+                    }
+                    catch (ex) {
+                        console.error('An error occurred while trying to apply the extension preferences: ' + 
+                            ex.toString());
+                    }
+                });
         }
-        catch (ex) {
-            console.error('An error while trying to set menu visibility: ' + ex.toString());
+
+        _warnIfDomIsDirty(event) {
+            if (this._domIsPure === false)
+                return event.returnValue = 'You discard all unsaved changes on this page when leaving.';
         }
-    });
+    
+        async _performStorageAction(callback) {
+            try {
+                await callback.bind(this)();
+    
+                this._canLoad = await this._pageInfo.canLoad();
+    
+                this._domIsPure = true;
+            }
+            finally {
+                MessageControl.hide();
+            }
+        }
 
-    const processMessage = async msg => {
-        try {
-            const receiver = new MessageReceiver(msg);
+        async _load() {
+            MessageControl.show('Page is loading');
+            await this._pageInfo.load();
+            
+            MessageControl.show('The page has been loaded successfully');
+        }
 
-            const curNode = activeNode;
-            activeNode = null;
-
-            let domWasChanged = false;
-
-            let outcome;
-
-            if (receiver.shouldMark())
-                domWasChanged = RangeMarker.markSelectedNodes(receiver.markColourClass);
-            else if (receiver.shouldUnmark()) {
-                domWasChanged = RangeMarker.unmarkSelectedNodes(curNode);
-                
-                if (!RangeMarker.domContainsMarkers()) {
-                    domWasChanged = false;
-                    domIsPure = true;
+        _setUpContextMenu(event) {
+            try {
+                if (event.button !== 2)
+                    return true;
+    
+                let msg;
+                const curColourClasses = RangeMarker.getColourClassesForSelectedNodes();
+    
+                const focusedNode = event.target;
+    
+                if (curColourClasses)
+                {
+                    msg = MessageReceiver.combineEvents(MessageReceiver.setMarkMenuReady(curColourClasses), 
+                        MessageReceiver.setAddNoteMenuReady());
+    
+                    if (curColourClasses.length)
+                        msg = MessageReceiver.combineEvents(msg, MessageReceiver.setUnmarkMenuReady());
                 }
+                else if (RangeMarker.isNodeMarked(focusedNode)) 
+                {
+                    msg = MessageReceiver.combineEvents(MessageReceiver.setUnmarkMenuReady(), 
+                        MessageReceiver.setAddNoteMenuReady());
+                    this._activeNode = focusedNode;
+                }
+                
+                if (RangeNote.hasNote(focusedNode)) {
+                    msg = MessageReceiver.combineEvents(msg, MessageReceiver.setRemoveNoteMenuReady());
+                    this._activeNode = focusedNode;
+                }
+                
+                msg = MessageReceiver.combineEvents(msg, MessageReceiver.addNoteLinks(RangeNote.getNoteLinks()));
+                this._browserApi.runtime.sendMessage(this._includeLoadSaveEvents(msg));
+    
+                this._sleep(10);
             }
-            else if (receiver.shouldChangeColour())
-                domWasChanged = RangeMarker.changeSelectedNodesColour(receiver.markColourClass, 
-                    curNode);
-            else if (receiver.shouldAddNote()) {
-                outcome = RangeNote.createNote(prompt('New note text:'), curNode);
-
-                if (outcome)
-                    domWasChanged = true;
+            catch (ex) {
+                console.error('An error while trying to set menu visibility: ' + ex.toString());
             }
-            else if (receiver.shouldRemoveNote()) {
-                outcome = RangeNote.removeNote(curNode);
-
-                if (outcome)
-                    domWasChanged = true;
-            }
-            else if (receiver.shouldSave())
-                await performStorageAction(save);
-            else if (receiver.shouldLoad())
-                await performStorageAction(load);
-            else if (receiver.shouldReturnTabState())
-                return includeLoadSaveEvents();
-            else if (receiver.shouldGoToNote())
-                RangeNote.goToNote(receiver.noteLink.id);
-            else
-                throw new Error(`The message '${JSON.stringify(msg)}' has a wrong format and cannot be processed`);
-
-            if (domWasChanged)
-                domIsPure = false;
-
-            return outcome;
         }
-        catch (err) {
-            console.error(err.toString());
-            throw err;
-        }
-    };
 
-    browserApi.runtime.onMessage(processMessage);
+        _includeLoadSaveEvents(msg = null) {
+            if (this._domIsPure)
+                return msg;
+    
+            if (this._domIsPure === false)
+                msg = MessageReceiver.combineEvents(msg, 
+                    MessageReceiver.setSaveMenuReady());
+    
+            if (this._canLoad)
+                msg = MessageReceiver.combineEvents(msg, 
+                    MessageReceiver.setLoadMenuReady());
+    
+            return msg;
+        }
+           
+        _sleep(milliseconds) {
+            const start = new Date().getTime();
+        
+            for (let i = 0; i < 1e7; i++)
+                if ((new Date().getTime() - start) > milliseconds)
+                    break;
+        }
+
+        async _processMessage(msg) {
+            try {
+                const receiver = new MessageReceiver(msg);
+    
+                const curNode = this._activeNode;
+                this._activeNode = null;
+    
+                let domWasChanged = false;
+    
+                let outcome;
+    
+                if (receiver.shouldMark())
+                    domWasChanged = RangeMarker.markSelectedNodes(receiver.markColourClass);
+                else if (receiver.shouldUnmark()) {
+                    domWasChanged = RangeMarker.unmarkSelectedNodes(curNode);
+                    
+                    if (!RangeMarker.domContainsMarkers()) {
+                        domWasChanged = false;
+                        this._domIsPure = true;
+                    }
+                }
+                else if (receiver.shouldChangeColour())
+                    domWasChanged = RangeMarker.changeSelectedNodesColour(receiver.markColourClass, 
+                        curNode);
+                else if (receiver.shouldAddNote()) {
+                    outcome = RangeNote.createNote(prompt('New note text:'), curNode);
+    
+                    if (outcome)
+                        domWasChanged = true;
+                }
+                else if (receiver.shouldRemoveNote()) {
+                    outcome = RangeNote.removeNote(curNode);
+    
+                    if (outcome)
+                        domWasChanged = true;
+                }
+                else if (receiver.shouldSave())
+                    await this._performStorageAction(this._save);
+                else if (receiver.shouldLoad())
+                    await this._performStorageAction(this._load);
+                else if (receiver.shouldReturnTabState())
+                    return this._includeLoadSaveEvents();
+                else if (receiver.shouldGoToNote())
+                    RangeNote.goToNote(receiver.noteLink.id);
+                else
+                    throw new Error(`The message '${JSON.stringify(msg)}' has a wrong format and cannot be processed`);
+    
+                if (domWasChanged)
+                    this._domIsPure = false;
+    
+                return outcome;
+            }
+            catch (err) {
+                console.error(err.toString());
+                throw err;
+            }
+        }
+
+        async _save() {
+            try {
+                await this._pageInfo.save();
+                MessageControl.show('The page has been saved successfully');
+            }
+            catch (err) {
+                alert(`An error occurred while trying to save the page: "${err.toString()}". ` +
+                    'Please, consider going to the preferences to remove redundant saved pages');
+            }
+        }
+    }
+
+    new Highlighter();
 }();
